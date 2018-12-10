4d6de3daecf58df8d4aacb99a6293605fe361fa6
Alastair <design@alastairbird.co.uk>
2018-12-10 15:38:07 +0000

    line of boxes rotating

--- js/box.js
+++ js/box.js
@@ -10,56 +10,64 @@ particle = (function() {
     	geometry,
     	material,
         boxOne,
-    	boxTwo,
+        boxTwo,
+        arrayOfBoxes,
+        numberOfBoxes,
         width,
         height;
 
     function init() {
+        // Vars
         width = window.innerWidth;
         height = window.innerHeight;
+        arrayOfBoxes = [];
+        numberOfBoxes = 10;
 
-        // New camera and position
-    	camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-		camera.position.z = 1;
-
-        // New scene
-		scene = new THREE.Scene();
-
-        // Create a box geometry & material
-		geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-		material = new THREE.MeshNormalMaterial();
-
-        // Apply the material to the box and add it to the scene
-        boxOne = new THREE.Mesh( geometry, material );
-		boxTwo = new THREE.Mesh( geometry, material );
-        boxOne.position.set(0.4,0.4,0.4);
-        scene.add( boxOne );
-		scene.add( boxTwo );
-
-        // Create a renderer and set its size
-		renderer = new THREE.WebGLRenderer( { antialias: true } );
-		renderer.setSize( width, height );
-	
-        // Add the renderer to the DOM
-    	document.body.appendChild( renderer.domElement );
-
-        // Listen to the window resize event
-        window.addEventListener('resize', onWindowResize);
-
-        // Fire animation
+        setupScene();
+        createBoxes();
         animate();
     }
 
-    function animate() {
+    function createBoxes() {
 
+        // Create a box geometry & material
+        geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        material = new THREE.MeshNormalMaterial();
+
+        for(var i=0; i < numberOfBoxes; i++){
+            arrayOfBoxes[i] = new THREE.Mesh( geometry, material );
+            scene.add( arrayOfBoxes[i] );
+            arrayOfBoxes[i].position.set(0.1 * i, 0, 0);
+        }
+    }
+
+    function setupScene() {
+        // New camera and position
+        camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 100 );
+        camera.position.z = 2;
+
+        // New scene
+        scene = new THREE.Scene();
+
+        // Create a renderer and set its size
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setSize( width, height );
+    
+        // Add the renderer to the DOM
+        document.body.appendChild( renderer.domElement );
+
+        // Listen to the window resize event
+        window.addEventListener('resize', onWindowResize);
+    }
+
+    function animate() {
         // Re-render everytime screen is refreshed
 		requestAnimationFrame( animate );
 
-        // Add some x and y rotation to the box
-		boxOne.rotation.x += 0.01;
-		boxOne.rotation.y += 0.02;
-        boxTwo.rotation.x += 0.02;
-        boxTwo.rotation.y += 0.01;
+        for(var i=0; i < arrayOfBoxes.length; i++){
+            arrayOfBoxes[i].rotation.y += 0.04;
+            arrayOfBoxes[i].rotation.x += 0.04;
+        }
 
         // Tell the renderer to add the scene and camera
 		renderer.render( scene, camera );
