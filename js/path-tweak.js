e8b2abdb336389fd7e3c38940d03c292f7d06116
Alastair <design@alastairbird.co.uk>
2018-12-12 20:47:21 +0000

    adds a second doughnut

---
+++ js/path-tweak.js
@@ -0,0 +1,134 @@
+
+var particle;
+
+particle = (function() {
+    'use strict';
+
+    var camera, 
+        scene,
+        renderer,
+        geometry,
+        material,
+        boxOne,
+        boxTwo,
+        arrayOfBoxes,
+        arrayOfBoxesTwo,
+        numberOfBoxes,
+        width,
+        time,
+        circleRadius,
+        cameraControls,
+        height;
+
+    function init() {
+        // Vars
+        width = window.innerWidth;
+        height = window.innerHeight;
+        arrayOfBoxes = [];
+        arrayOfBoxesTwo = [];
+        numberOfBoxes = 800;
+        time = 0;
+        circleRadius = 3;
+
+        setupScene();
+        createBoxes();
+        animate();
+    }
+
+    function createBoxes() {
+
+        // Create a box geometry & material
+        material = new THREE.MeshNormalMaterial();
+
+        for(var i=0; i < numberOfBoxes; i++){
+            // geometry = new THREE.BoxGeometry( Math.random(0,0.3), Math.random(0,0.3), Math.random(0,0.3) );
+            geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+            arrayOfBoxes[i] = new THREE.Mesh( geometry, material );
+            scene.add( arrayOfBoxes[i] );
+            arrayOfBoxes[i].position.set(0,0,0);
+
+            arrayOfBoxesTwo[i] = new THREE.Mesh( geometry, material );
+            scene.add( arrayOfBoxesTwo[i] );
+            arrayOfBoxesTwo[i].position.set(0,0,0);
+        }
+
+    }
+
+    function setupScene() {
+        // New camera and position
+        camera = new THREE.PerspectiveCamera( 90, width / height, 0.01, 100 );
+        camera.position.z = 5;
+        camera.position.x = 0;
+        camera.position.y = 0;
+        cameraControls = new THREE.OrbitControls( camera );
+
+        // New scene
+        scene = new THREE.Scene();
+
+        // Create a renderer and set its size
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setSize( width, height );
+    
+        // Add the renderer to the DOM
+        document.body.appendChild( renderer.domElement );
+
+        // Listen to the window resize event
+        window.addEventListener('resize', onWindowResize);
+    }
+
+    function animate() {
+        // Re-render everytime screen is refreshed
+        requestAnimationFrame( animate );
+
+        // Increase time
+        time += 0.01;
+
+        for(var i=0; i < arrayOfBoxes.length; i++){
+            arrayOfBoxes[i].rotation.y += 0.02;
+            arrayOfBoxes[i].rotation.x = Math.cos(i * 3);
+            arrayOfBoxes[i].rotation.z = Math.sin(i * 2);
+
+
+            arrayOfBoxesTwo[i].rotation.x = Math.cos(i * 3);
+            arrayOfBoxesTwo[i].rotation.z = Math.sin(i * 2);
+            // arrayOfBoxes[i].rotation.z += 0.05;
+            // arrayOfBoxes[i].position.x = Math.cos(time) + (Math.cos(time));
+            // arrayOfBoxes[i].position.z = Math.sin(time) + (Math.sin(time) * i);
+            // angle = Math.cos(i)
+
+           
+            arrayOfBoxesTwo[i].position.x = (Math.sin(time + i) * circleRadius / 2);
+            arrayOfBoxesTwo[i].position.z = (Math.cos(time + i) * circleRadius / 2);
+
+            arrayOfBoxes[i].position.x = (Math.sin(time + i) * circleRadius);
+            arrayOfBoxes[i].position.y = (Math.cos(time + i) * circleRadius);
+        }
+
+        // Tell the renderer to add the scene and camera
+        cameraControls.update();
+        renderer.render( scene, camera );
+        updateRendererSize();
+    }
+
+    function onWindowResize(){
+        width = window.innerWidth;
+        height = window.innerHeight;
+        updateRendererSize();
+    }
+    
+    // Change the size of the renderer and keep aspect ratio
+    function updateRendererSize(){
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    }
+      
+    return {
+        init: init,
+    };
+
+}());
+
+
+particle.init();
